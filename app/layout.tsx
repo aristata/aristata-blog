1217490a2e027f13521611b14ffaf5b49c26109a
aristata <aristatait@gmail.com>
2023-10-25 14:39:52 +0900

    feat: 스크롤시 네브바 배경색 바뀌도록 함

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
-"use client"
+"use client";
 
-import "./globals.css"
-import NavBar from "@/components/NavBar"
-import Footer from "@/components/Footer"
-import Container from "@/components/Container"
-import Analytics from "@/components/Analytics"
-import { Metadata } from "next"
+import "./globals.css";
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
+import Container from "@/components/Container";
+import Analytics from "@/components/Analytics";
+import { Metadata } from "next";
 
 const metadata: Metadata = {
   title: "Aristata's Blog",
   description: "일상, 기술, 포트폴리오 등을 게재할 블로그 입니다",
   authors: [{ name: "Aristata" }]
-}
+};
 
 interface RootLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
@@ -27,5 +27,5 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <Footer />
       </body>
     </html>
-  )
+  );
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,50 +1,106 @@
-"use client"
+"use client";
 
-import Link from "next/link"
-import ThemeIcon from "./ThemeIcon"
-import HomeSVG from "./svgs/Home"
-import IdentificationSVG from "./svgs/Identification"
-import InboxesSVG from "./svgs/Inboxes"
-import { useEffect, useState } from "react"
+import Link from "next/link";
+import ThemeIcon from "./ThemeIcon";
+import HomeSVG from "./svgs/Home";
+import IdentificationSVG from "./svgs/Identification";
+import InboxesSVG from "./svgs/Inboxes";
+import { useEffect, useState } from "react";
+import {
+  motion,
+  useAnimation,
+  useMotionValueEvent,
+  useScroll
+} from "framer-motion";
+
+const scrollAnimationVariants = {
+  top: {
+    backgroundColor: "rgba(255, 255, 255, 1)"
+  },
+  darkTop: {
+    backgroundColor: "rgba(17, 17, 17, 1)"
+  },
+  scroll: {
+    backgroundColor: "rgb(255, 255, 255, 1)"
+  },
+  darkScroll: {
+    backgroundColor: "rgba(17, 17, 17, 1)"
+  }
+};
 
 const NavBar = () => {
   /*************************************************************************************************
    * 테마
    *************************************************************************************************/
-  const [theme, setTheme] = useState("light")
+  const [theme, setTheme] = useState("light");
   const setDarkTheme = () => {
-    document.documentElement.classList.add("dark")
-    localStorage.setItem("theme", "dark")
-    setTheme("dark")
-  }
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+    setTheme("dark");
+  };
   const setLightTheme = () => {
-    document.documentElement.classList.remove("dark")
-    localStorage.setItem("theme", "light")
-    setTheme("light")
-  }
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+    setTheme("light");
+  };
   useEffect(() => {
-    const localTheme = localStorage.getItem("theme")
+    const localTheme = localStorage.getItem("theme");
     if (
       localTheme === "dark" ||
       (localTheme === null &&
         window.matchMedia("(prefers-color-scheme: dark)").matches)
     ) {
-      setDarkTheme()
+      setDarkTheme();
     } else {
-      setLightTheme()
+      setLightTheme();
     }
-  }, [])
+  }, []);
   const handleTheme = () => {
     if (theme === "dark") {
-      setLightTheme()
+      setLightTheme();
+      navAnimation.start("top");
     } else {
-      setDarkTheme()
-    }
-  }
+      setDarkTheme();
 
+      navAnimation.start("darkTop");
+    }
+  };
+
+  /*************************************************************************************************
+   * 스크롤 이벤트
+   *
+   * - useAnimation 을 사용해서 navAnimation 만들기
+   * - useViewportScroll 을 사용해서 scrollY 값 가져오기
+   *   - useViewportScroll 은 deprecated 되었다
+   *   - useScroll 을 사용할 것
+   *************************************************************************************************/
+  const navAnimation = useAnimation();
+  const { scrollY } = useScroll();
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    // console.log("scrollY: " + latest);
+    // console.log("theme: " + theme);
+    if (latest > 0) {
+      if (theme === "light") {
+        navAnimation.start("scroll");
+      } else {
+        navAnimation.start("darkScroll");
+      }
+    } else {
+      if (theme === "light") {
+        navAnimation.start("top");
+      } else {
+        navAnimation.start("darkTop");
+      }
+    }
+  });
   return (
     <>
-      <header className="sticky top-0 mx-auto max-w-2xl flex justify-center items-center gap-x-20 mt-10">
+      <motion.nav
+        variants={scrollAnimationVariants}
+        animate={navAnimation}
+        initial={theme === "light" ? "top" : "darkTop"}
+        className="sticky top-0 mx-auto max-w-2xl flex justify-center items-center gap-x-20 mt-10"
+      >
         {/* 로고 */}
         <Link href={"/"}>
           <div className="p-4">
@@ -67,9 +123,9 @@ const NavBar = () => {
         <button onClick={handleTheme} className="p-4">
           <ThemeIcon theme={theme} />
         </button>
-      </header>
+      </motion.nav>
     </>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
